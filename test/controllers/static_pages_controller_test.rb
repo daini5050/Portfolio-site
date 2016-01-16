7b6c41aa1011adcfc8d7710220ad99d0802f5fe4
Daini Sani <daini5050@live.com>
2016-01-16 16:51:54 +0200

    add dynamic page title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,21 +4,28 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Daini Sani"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Daini Sani"
   end
 
   test "should get work" do
     get :work
     assert_response :success
+    assert_select "title", "Work | Daini Sani"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | Daini Sani"
   end
 
 end
+
+
+
