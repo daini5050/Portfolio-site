5f8498fb99f7cf6e8fba0fee871ce4e82c6317b6
Daini Sani <daini5050@live.com>
2016-01-20 14:56:23 +0200

    Added social links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'static_pages#about'
 
-  get 'contact' => 'static_pages#contact'
+  get 'contact' => 'contacts#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
